25c59d0a48508c517bb42a992334123197444ef0
Charles BEYER <charles.beyer.pro@gmail.com>
2020-05-07 19:37:54 +0200

    feat(contract): updated contract and tests

--- packages/smart-contract/test/smartwalletmanager.ts
+++ packages/smart-contract/test/smartwalletmanager.ts
@@ -5,103 +5,99 @@ import { deployContract, solidity } from 'ethereum-waffle';
 import SmartWalletManagerArtifact from '../artifacts/SmartWalletManager.json';
 import { SmartWalletManager } from '../typechain/SmartWalletManager';
 import UserSmartWalletArtifact from '../artifacts/UserSmartWallet.json';
-
-const ZERO = '0x0000000000000000000000000000000000000000';
-const UID = 'u-123';
+import { TransactionResponse } from 'ethers/providers';
+// import { UserSmartWallet } from '../typechain/UserSmartWallet'
 
 chai.use(solidity);
 const { expect } = chai;
+const ZERO = '0x0000000000000000000000000000000000000000';
 
 describe('SmartWalletManager', () => {
   let signers: Signer[];
   let smartWalletManager: SmartWalletManager;
-
-  beforeEach(async () => {
+  // let userSmartWallet: UserSmartWallet;
+  before(async () => {
     signers = await ethers.signers();
     smartWalletManager = (await deployContract(
-      <Wallet>signers[0],
+      <Wallet>signers[0], // `wallet` : If you need to test the contract with a mocked provider
       SmartWalletManagerArtifact,
     )) as SmartWalletManager;
-
-    expect(smartWalletManager.address).to.properAddress;
-    expect(await smartWalletManager.owner()).to.eq(await signers[0].getAddress());
   });
 
-  it('create a smart-wallet', async () => {
-    const address = await createWallet(smartWalletManager);
-    expect(address).to.not.eq(ZERO);
-    // await expect(contract.method()).to.be.reverted; To-do
-  });
-
-  it('fund a smart-wallet', async () => {
-    const address = await createWallet(smartWalletManager);
-    const amount = 1;
-    const tx = await signers[0].sendTransaction({
-      to: address,
-      value: ethers.utils.parseEther(amount.toString()),
+  describe('Deployment', async () => {
+    it('deploy with a proper contract address', async () => {
+      expect(smartWalletManager.address).to.properAddress;
     });
-    expect(tx.blockNumber).to.be.greaterThan(0);
-
-    const userSmartWallet = await ethers.getContractAt(UserSmartWalletArtifact.abi, address);
-    expect(await userSmartWallet.owner()).to.eq(await smartWalletManager.address);
-    expect((await userSmartWallet.getBalance()).toString()).to.eq(
-      await ethers.utils.parseEther(amount.toString()),
-    );
+    it('set the right owner', async () => {
+      expect(await smartWalletManager.owner()).to.eq(await signers[0].getAddress());
+    });
   });
 
-  it('fund a smart-wallet and intercept the first emitted event', async () => {
-    const address = await createWallet(smartWalletManager);
-    const amount = 1;
+  describe('Smart-wallet', async () => {
+    const UID = '123456789';
+    let smartWalletAddress = ZERO;
+    const amountToFund = '1';
 
-    new Promise((resolve, reject) => {
-      smartWalletManager.addListener(
-        'FundedAccountAddress',
-        (UIDEmitted: any, userAccountAddressEmitted: any, event: any) => {
-          expect(UIDEmitted).to.eq(UID);
-          expect(userAccountAddressEmitted).to.eq(address);
-          event.removeListener();
-          resolve();
-        },
+    it('create a smart-wallet', async () => {
+      expect(await smartWalletManager.getWalletAddress(UID)).to.eq(ZERO);
+      await smartWalletManager.createWallet(UID);
+      smartWalletAddress = await smartWalletManager.getWalletAddress(UID);
+      expect(smartWalletAddress).to.not.eq(ZERO);
+    });
+
+    it('fund a smart-wallet', async () => {
+      const tx: TransactionResponse = await signers[0].sendTransaction({
+        to: smartWalletAddress,
+        value: ethers.utils.parseEther(amountToFund),
+      });
+      expect(tx.blockNumber).to.be.greaterThan(0);
+      // const userSmartWallet = await ethers.getContractAt('UserSmartWallet', smartWalletAddress);
+      const userSmartWallet = await ethers.getContractAt(
+        UserSmartWalletArtifact.abi,
+        smartWalletAddress,
+      );
+      expect(await userSmartWallet.owner()).to.eq(await smartWalletManager.address);
+      expect((await userSmartWallet.getBalance()).toString()).to.eq(
+        await ethers.utils.parseEther(amountToFund),
       );
     });
 
-    const tx = await signers[0].sendTransaction({
-      to: address,
-      value: ethers.utils.parseEther(amount.toString()),
+    it('fund a smart-wallet and intercept the first emitted event', async () => {
+      await expect(
+        signers[1].sendTransaction({
+          to: smartWalletAddress,
+          value: ethers.utils.parseEther(amountToFund),
+        }),
+      )
+        .to.emit(smartWalletManager, 'FundedAccountAddress')
+        .withArgs(UID, smartWalletAddress);
     });
 
-    expect(tx.blockNumber).to.be.greaterThan(0);
-  });
+    it('fund a smart-wallet and intercept the second emitted event', async () => {
+      await expect(
+        signers[2].sendTransaction({
+          to: smartWalletAddress,
+          value: ethers.utils.parseEther(amountToFund),
+        }),
+      )
+        .to.emit(smartWalletManager, 'FundedAccountAmount')
+        .withArgs(UID, ethers.utils.parseEther(amountToFund));
+    });
 
-  it('fund a smart-wallet and intercept the second emitted event', async () => {
-    const address = await createWallet(smartWalletManager);
-    const amount = 1;
-
-    new Promise((resolve, reject) => {
-      smartWalletManager.addListener(
-        'FundedAccountAmount',
-        (UIDEmitted: any, amountEmitted: any, event: any) => {
-          expect(UIDEmitted).to.eq(UID);
-          expect(amountEmitted).to.eq(ethers.utils.parseEther(amount.toString()));
-          event.removeListener();
-          resolve();
-        },
+    it('can not create a smart-wallet to the same user twice', async () => {
+      expect(await smartWalletManager.getWalletAddress(UID)).to.eq(smartWalletAddress);
+      await expect(smartWalletManager.createWallet(UID)).to.be.revertedWith(
+        'The uid already has a wallet.',
       );
     });
 
-    const tx = await signers[0].sendTransaction({
-      to: address,
-      value: ethers.utils.parseEther(amount.toString()),
+    it('can not create a smart-wallet if the caller is not the owner', async () => {
+      expect(await smartWalletManager.getWalletAddress(UID)).to.eq(smartWalletAddress);
+      let newUID = '987654321';
+      const nonAdminSmartWalletManager = smartWalletManager.connect(<Wallet>signers[1]);
+      await expect(nonAdminSmartWalletManager.createWallet(newUID)).to.be.revertedWith(
+        'Caller must be the owner.',
+      );
     });
-
-    expect(tx.blockNumber).to.be.greaterThan(0);
   });
 });
-
-const createWallet = async (smartWalletManager: SmartWalletManager) => {
-  expect(await smartWalletManager.getWalletAddress(UID)).to.eq(ZERO);
-  await smartWalletManager.createWallet(UID);
-  const address = await smartWalletManager.getWalletAddress(UID);
-
-  return address;
-};
